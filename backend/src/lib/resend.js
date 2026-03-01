768ead97b844fd55e21f2286822e2a1118bd3903
rohansuradkar01 <rohansuradkar01@gmail.com>
2026-03-01 19:56:56 +0530

    first commit

---
+++ backend/src/lib/resend.js
@@ -0,0 +1,13 @@
+import { Resend } from "resend";
+import { ENV } from "./env.js";
+
+
+
+console.log(ENV.RESEND_API_KEY);
+export const resendClient = new Resend(ENV.RESEND_API_KEY);
+
+export const sender = {
+  email: ENV.EMAIL_FROM,
+  name: ENV.EMAIL_FROM_NAME,
+};
+
